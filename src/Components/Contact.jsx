7c5d152e4f8d71dbdcdf63914ddfdd4f01f888c7
Issa <JeanMarieIssa@gmail.com>
2021-02-11 13:28:02 +0300

    Tooltip design

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -33,6 +33,45 @@ class Contact extends React.Component {
 			overflow: hidden;
 		`;
 
+		const A = styled.a`
+			color: none;
+			text-decoration: none;
+
+		`;
+
+		const Tip = styled.div`
+			text-decoration: none;
+			font-size: 0.9em;
+			margin: 2px;
+			background: white;
+			border-radius: 4px;
+			position: relative;
+			margin-bottom: 10px;
+			color: rgb(100,100,100);
+			height: 30px;
+			padding: 6px;
+			opacity: 0;
+
+			&::after{
+				background: white;
+				color: rgb(100,100,100);
+				content: '.';
+				display: block;
+				width: 10px;
+				hight: 10px;
+				position: absolute;
+				bottom: -10px;
+				left: 42%;
+				border-bottom-left-radius: 50px;
+				border-bottom-right-radius: 50px;
+			}
+
+			@media screen and (max-width: 768px) {
+				display: none;
+			}
+		`;
+
+
 		return (
 			<Contact className="welcome" id="contact">
 
@@ -44,7 +83,7 @@ class Contact extends React.Component {
 				<br />
 
 				<p>
-					Get to me through the phone number below, <br />
+					You can reach through the phone number below, <br />
 					Or the icons below. <br />
 				</p>
 				<br /> <br /><br />
@@ -55,24 +94,30 @@ class Contact extends React.Component {
 
 				<br />
 				<div className="cont-icons">
-					<a href="tel: +250788734295">
+					<A className="a-1" href="tel: +250788734295" title="Mobile Call">
+						<Tip className="tip">Phone call</Tip>
 						<div className="icon-1 fas fa-phone">
-					</div></a>
-					<a href="mailto: jeanmarieissa@gmail.com">
+					</div></A>
+					<A className="a-2" href="mailto: jeanmarieissa@gmail.com" title="Email">
+						<Tip className="tip">Email</Tip>
 						<div className="icon-2 fas fa-envelope"></div>
-					</a>
-					<a href="https://wa.me/+250788734295">
+					</A>
+					<A className="a-3" href="https://wa.me/+250788734295" title="WhatsApp">
+						<Tip className="tip">WhatsApp</Tip>
 						<div className="icon-3 fab fa-whatsapp"></div>
-					</a>
-					<a href="https://github.com/issajeanmarie/">
+					</A>
+					<A className="a-4" href="https://github.com/issajeanmarie/" title="GitHub">
+						<Tip className="tip">GitHub</Tip>
 						<div className="icon-4 fab fa-github"></div>
-					</a>
-					<a href="https://linkedin.com/in/issa-jean-marie-509660151">
+					</A>
+					<A className="a-5" href="https://linkedin.com/in/issa-jean-marie-509660151" title="Linkedin">
+						<Tip className="tip">Linkedin</Tip>
 						<div className="icon-5 fab fa-linkedin"></div>
-					</a>
-					<a href="https://www.instagram.com/codes.study/">
+					</A>
+					<A className="a-6" href="https://www.instagram.com/codes.study/" title="Instagram">
+						<Tip className="tip">Instagram</Tip>
 						<div className="icon-6 fab fa-instagram"></div>
-					</a>
+					</A>
 				</div>
 				
 			</Contact>
