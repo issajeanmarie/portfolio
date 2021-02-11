41bdb6b5f3e080d4f283aebe7ef8bc914d133cb0
Issa <JeanMarieIssa@gmail.com>
2021-02-11 18:36:02 +0300

    Centering Icons

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -59,27 +59,27 @@ class Welcome extends React.Component {
 				<br />
 
 				<div className="cont-icons">
-					<A className="a-1" href="tel: +250788734295" title="Mobile Call">
+					<A className="a-1" href="tel: +250788734295">
 						<Tip className="tip">Phone call</Tip>
 						<div className="icon-1 fas fa-phone">
 					</div></A>
-					<A className="a-2" href="mailto: jeanmarieissa@gmail.com" title="Email">
+					<A className="a-2" href="mailto: jeanmarieissa@gmail.com">
 						<Tip className="tip">Email</Tip>
 						<div className="icon-2 fas fa-envelope"></div>
 					</A>
-					<A className="a-3" href="https://wa.me/+250788734295" title="WhatsApp">
+					<A className="a-3" href="https://wa.me/+250788734295">
 						<Tip className="tip">WhatsApp</Tip>
 						<div className="icon-3 fab fa-whatsapp"></div>
 					</A>
-					<A className="a-4" href="https://github.com/issajeanmarie/" title="GitHub">
+					<A className="a-4" href="https://github.com/issajeanmarie/">
 						<Tip className="tip">GitHub</Tip>
 						<div className="icon-4 fab fa-github"></div>
 					</A>
-					<A className="a-5" href="https://linkedin.com/in/issa-jean-marie-509660151" title="Linkedin">
+					<A className="a-5" href="https://linkedin.com/in/issa-jean-marie-509660151">
 						<Tip className="tip">Linkedin</Tip>
 						<div className="icon-5 fab fa-linkedin"></div>
 					</A>
-					<A className="a-6" href="https://www.instagram.com/codes.study/" title="Instagram">
+					<A className="a-6" href="https://www.instagram.com/codes.study/">
 						<Tip className="tip">Instagram</Tip>
 						<div className="icon-6 fab fa-instagram"></div>
 					</A>
